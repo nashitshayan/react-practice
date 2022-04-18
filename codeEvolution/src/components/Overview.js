76fc672d5a8b63d3fa3003705a642956e99aca07
Nashit Shayan <shayan.nashit@gmail.com>
2022-04-18 09:09:02 +0530

    trying edit feat

--- codeEvolution/src/components/Overview.js
+++ codeEvolution/src/components/Overview.js
@@ -1,29 +1,76 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import {
+	faTrash,
+	faEdit,
+	faCircleCheck,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function Overview(props) {
-	const { tasks, numberOfTasks, deleteTask } = props;
+	const { tasks, deleteTask, isEditing, editTask, saveTask, taskInput } = props;
+
+	const handleTaskInput = (e) => {
+		// taskInput(e);
+	};
 	const handleDeleteTask = (e) => {
 		let id = e.target.closest('.icon').dataset.id;
 		deleteTask(id);
 	};
+
+	const handleEditTask = (e) => {
+		let id = e.target.closest('.icon').dataset.id;
+		let text = e.target.closest('.icon').parentElement.firstChild;
+		console.log('edit', text);
+		// editTask(id, text);
+	};
+	const handleSaveTask = (e) => {
+		let id = e.target.closest('.icon').dataset.id;
+		let text = e.target.closest('.icon').parentElement.firstChild;
+		console.log('save', text);
+		//saveTask();
+	};
+	const taskList = tasks.map((task) => {
+		if (isEditing) {
+			return (
+				<p>
+					<input
+						type='text'
+						placeholder='type'
+						onChange={handleTaskInput}
+						value={task.text}
+					/>
+					<FontAwesomeIcon
+						icon={faCircleCheck}
+						onClick={handleSaveTask}
+						className='icon'
+						data-id={task.id}
+					/>
+				</p>
+			);
+		} else {
+			return (
+				<li key={task.id}>
+					{task.text}{' '}
+					<FontAwesomeIcon
+						icon={faTrash}
+						onClick={handleDeleteTask}
+						className='icon'
+						data-id={task.id}
+					/>{' '}
+					<FontAwesomeIcon
+						icon={faEdit}
+						onClick={handleEditTask}
+						className='icon'
+						data-id={task.id}
+					/>
+				</li>
+			);
+		}
+	});
 	return (
 		<>
 			{/* <span>{numberOfTasks}</span> */}
-			<ul>
-				{tasks.map((task) => (
-					<li key={task.id}>
-						{task.text}{' '}
-						<FontAwesomeIcon
-							icon={faTrash}
-							onClick={handleDeleteTask}
-							className='icon'
-							data-id={task.id}
-						/>
-					</li>
-				))}
-			</ul>
+			<ul>{taskList}</ul>
 		</>
 	);
 }
--- codeEvolution/src/components/Tasks.js
+++ codeEvolution/src/components/Tasks.js
@@ -9,9 +9,10 @@ class Tasks extends Component {
 			task: {
 				text: '',
 				id: uniqid(),
+				// isEditing: false,
 			},
 			tasks: [],
-			numberOfTasks: 0,
+			//numberOfTasks: 0,
 		};
 	}
 	handleTaskInput = (e) => {
@@ -19,6 +20,7 @@ class Tasks extends Component {
 			task: {
 				text: e.target.value,
 				id: this.state.task.id,
+				// isEditing: true,
 			},
 		});
 	};
@@ -29,7 +31,7 @@ class Tasks extends Component {
 				text: '',
 				id: uniqid(),
 			},
-			numberOfTasks: this.state.numberOfTasks + 1,
+			//numberOfTasks: this.state.numberOfTasks + 1,
 		});
 		e.preventDefault();
 	};
@@ -39,11 +41,25 @@ class Tasks extends Component {
 			tasks: this.state.tasks.filter((task) => task.id !== id),
 		});
 	};
+	// handleEditTask = (id, text) => {
+	// 	this.setState({
+	// 		task: {
+	// 			text: text,
+	// 			id: id,
+	// 			isEditing: true,
+	// 		},
+	// 	});
+	// };
+	// handleSaveTask = () => {
+	// 	this.setState({
+	// 		isEditing: false,
+	// 	});
+	// };
 	// componentDidUpdate() {
 	// 	// console.log(this.state.tasks.filter((task) => task.id !== '"l22rdf9d"'));
 	// }
 	render() {
-		const { task, tasks, numberOfTasks } = this.state;
+		const { task, tasks, numberOfTasks, isEditing } = this.state;
 		const box = {
 			border: '1px solid black',
 			backgroundColor: 'beige',
@@ -64,6 +80,10 @@ class Tasks extends Component {
 					tasks={tasks}
 					numberOfTasks={numberOfTasks}
 					deleteTask={this.handleDeleteTask}
+					// isEditing={isEditing}
+					// editTask={this.handleEditTask}
+					// saveTask={this.handleSaveTask}
+					// taskInput={this.handleTaskInput}
 				/>
 			</div>
 		);
